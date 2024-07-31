82da7bdb20ac66c96493e43fa09be20e5dd2cfc6
Rishy-2021 <riteshpuwar2001@gmail.com>
2024-07-31 14:58:00 +0530

    _

--- server/index.ts
+++ server/index.ts
@@ -9,8 +9,8 @@ dotenv.config();
 connectDB();
 
 const corsOptions: cors.CorsOptions = {
-  origin: ["http://localhost:3000", "https://farmlink-client.vercel.app", "https://www.foodstarter-nft.io"],
-  credentials: true,
+  origin: ["http://localhost:3000"],
+  optionsSuccessStatus: 200 // For legacy browser support
 };
 
 const app: Application = express();
@@ -18,15 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin",["http://localhost:3000"]);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Expose-Headers", "Set-Cookie");
-  next();
-});
 
 const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
