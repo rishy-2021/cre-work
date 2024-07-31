c3332e67fb8a26b495bb33c6ebabeafbc93b16aa
Rishy-2021 <riteshpuwar2001@gmail.com>
2024-07-31 14:48:16 +0530

    set cors

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin");
+  res.header("Access-Control-Allow-Origin",["http://localhost:3000"]);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
