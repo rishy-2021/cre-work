0f89886a5d0b18b870677462590e6a2bfefaceba
Rishy-2021 <riteshpuwar2001@gmail.com>
2024-07-31 15:10:48 +0530

    _

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction, Application } from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectDB from './src/config/db';
@@ -10,24 +10,20 @@ connectDB();
 
 const app: Application = express();
 
+// CORS configuration
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: "http://localhost:3000",
   credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+  exposedHeaders: "Set-Cookie",
+  optionsSuccessStatus: 204 // For legacy browser support
 };
 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", 'true');
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Expose-Headers", "Set-Cookie");
-  next();
-});
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
