53c95cc60fc9a2e984065908abc662ce879c9d7b
Ritesh Purwar <riteshpuwar2001@gmail.com>
2024-08-02 07:23:33 +0000

    _

--- server/src/utils/allowCors.ts
+++ server/src/utils/allowCors.ts
@@ -8,7 +8,7 @@ const allowCors = (fn: Handler): Handler => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   // You could also set this to a specific origin:
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
