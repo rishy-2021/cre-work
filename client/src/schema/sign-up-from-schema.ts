1e780d6e5307ae6cef342c2acae499277eb77942
Ritesh Purwar <riteshpuwar2001@gmail.com>
2024-08-05 18:23:48 +0000

    delete unused files

--- client/src/schema/sign-up-from-schema.ts
+++
@@ -1,9 +0,0 @@
-import { LoginFormData, SignupFormData } from "@/types/auth/types";
-import { z, ZodType } from "zod";
-
-export const signUpSchema: ZodType<SignupFormData> = z
-  .object({
-    name:z.string().min(2, "Please enter a vaild name"),
-    email: z.string().email(),
-    password: z.string().min(8, "Password must contain at least 8 characters").max(128)
-  });
